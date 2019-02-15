b1a3169f01be4e7d4d1e026059f2b6a70d5737a7
Laurent Bovet <laurent.bovet@windmaster.ch>
2019-02-15 11:22:01 +0100

    Offset reset. WIP

--- src/test/java/io/streap/kafka/ReactorKafkaTest.java
+++ src/test/java/io/streap/kafka/ReactorKafkaTest.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.sender.KafkaSender;
@@ -14,6 +16,7 @@ import reactor.kafka.sender.SenderRecord;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static io.streap.test.EmbeddedKafkaSupport.receiverOptions;
 import static io.streap.test.EmbeddedKafkaSupport.senderOptions;
@@ -81,5 +84,47 @@ public class ReactorKafkaTest {
         assertEquals(0L, latch.getCount());
     }
 
+    @Test
+    public void testConsumeAgain() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(2);
+        AtomicBoolean shouldStop = new AtomicBoolean(true);
+        StringBuffer result = new StringBuffer();
 
+        KafkaSender sender = KafkaSender.create(senderOptions()
+                .producerProperty(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "SampleTxn"));
+
+        KafkaReceiver<Integer, String> receiver = KafkaReceiver.create(receiverOptions("consume.again.In"));
+        receiver
+                .receiveExactlyOnce(sender.transactionManager())
+                .doOnNext(m -> System.out.println("Received batch:" + m))
+                .concatMap(f -> f
+                    .doOnNext(i -> System.out.println("Received item:" + i.value()))
+                    .doOnNext(i -> {
+                        if (shouldStop.getAndSet(false)) {
+                            throw new RuntimeException("Aborted when item 1 is first seen");
+                        }
+                    })
+                    .doOnNext(i -> result.append(i.value()))
+                    .doOnNext(i -> latch.countDown())
+                    .onErrorResume(e ->
+                        receiver.doOnConsumer( consumer -> consumer.assignment()
+                                .forEach(tp -> consumer.seek(tp, consumer.committed(tp).offset())))
+                                .then(sender.transactionManager().abort())
+                        ))
+                .subscribe();
+
+        Thread.sleep(500);
+
+        KafkaSender.create(senderOptions())
+                .send(Flux.range(0, 2)
+                        .map(x -> SenderRecord.create("consume.again.In", null, null, 1, "hello-" + x, 1)))
+                .then()
+                .doOnError(Throwable::printStackTrace)
+                .doOnSuccess(s -> System.out.println("Sent"))
+                .subscribe();
+
+        latch.await(7, TimeUnit.SECONDS);
+        assertEquals(0L, latch.getCount());
+        assertEquals("hello-0hello-1hello-2", result.toString());
+    }
 }
